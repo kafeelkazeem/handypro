b99a44cc7775eb42755c80d8ce869710d2fd9473
kafe181 <kafeelkazeem@gmail.com>
2024-02-14 15:33:59 +0100

    achieved login

--- src/component/pages/login.jsx
+++ src/component/pages/login.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const LogForm = () =>{
@@ -12,6 +14,25 @@ const LogForm = () =>{
         fontWeight: 'bold',
         border: '1px solid #03580e'
     }
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const onSubmit =  () =>{
+      const url = 'http://localhost/handypro/login.php'
+      let fData = new FormData()
+      fData.append('eml', email)
+      fData.append('pswd', password)
+      axios.post(url, fData)
+      .then(res =>{
+          if(res.data.link == 'true'){
+            navigate('/home')
+          }
+      })  
+      .catch(error=>alert(error))
+    }
+
     return(
         <Container fluid style={cont} className="vh-100 d-flex align-items-center justify-content-center rounded">
         <Row>
@@ -23,6 +44,8 @@ const LogForm = () =>{
                   <Form.Label className='text-white'>Email address</Form.Label>
                   <Form.Control
                     type="email"
+                    value = {email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                   />
                 </Form.Group>
@@ -31,12 +54,14 @@ const LogForm = () =>{
                   <Form.Label className='text-white'>Password</Form.Label>
                   <Form.Control
                     type="password"
+                    value = {password}
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                   />
                 </Form.Group>
     
                 <div className="d-grid gap-2" style={{marginTop: '2rem'}}>
-                    <Button style={button} size="lg">
+                   <Button onClick = {onSubmit} style={button} size="lg">
                         Login
                     </Button>
                 </div>
