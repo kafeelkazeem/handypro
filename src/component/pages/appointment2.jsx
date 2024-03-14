a61181dd2ccf59882b420cb3b0c82b8e951e91b9
Kafeel Kazeem <kafeelkazeem@gmail.com>
2024-03-14 11:47:01 +0100

    made changes

---
+++ src/component/pages/appointment2.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import MyNav from '../navbar'
+
+const Container = () =>{
+  const style = { 
+      backgroundColor: '#212515',
+      overflow: 'auto',
+      height: '100vh'
+  }
+  return(
+      <>
+        <div className='container-fluid p-5 pt-5' style={style}>
+          <h1 style={{color: '#fff', fontSize: '2rem'}}>About Us</h1>
+          <p style={{color: '#fff', fontSize: '1.3rem'}}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est eaque enim soluta officia. Sint nemo ipsam corrupti, quisquam beatae minima, dicta, expedita tenetur quis consequuntur sapiente voluptatum omnis veniam nesciunt!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est eaque enim soluta officia. Sint nemo ipsam corrupti, quisquam beatae minima, dicta, expedita tenetur quis consequuntur sapiente voluptatum omnis veniam nesciunt!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est eaque enim soluta officia. Sint nemo ipsam corrupti, quisquam beatae minima, dicta, expedita tenetur quis consequuntur sapiente voluptatum omnis veniam nesciunt!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est eaque enim soluta officia. Sint nemo ipsam corrupti, quisquam beatae minima, dicta, expedita tenetur quis consequuntur sapiente voluptatum omnis veniam nesciunt!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est eaque enim soluta officia. Sint nemo ipsam corrupti, quisquam beatae minima, dicta, expedita tenetur quis consequuntur sapiente voluptatum omnis veniam nesciunt!
+          </p>
+        </div>
+      </>
+  )
+}
+
+const Appointment2 = () => {
+  return (
+    <>
+      <MyNav />
+      <Container />
+    </>
+  )
+}
+
+export default Appointment2
